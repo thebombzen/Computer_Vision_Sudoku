b1dedaa67971ef38a23702545cbac2d8fc59c599
Max Cowan <maxace@roadrunner.com>
2017-04-17 12:44:00 -0400

    Run

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Max Cowan on 3/4/17.
+//
+
+#include "SudokuSolver.h"
+#include "VisualProcessor.h"
+
+using namespace std;
+
+// Begin frame-by-frame analysis
+int main() {
+
+    VisualProcessor processor = VisualProcessor();
+
+    return 0;
+}
